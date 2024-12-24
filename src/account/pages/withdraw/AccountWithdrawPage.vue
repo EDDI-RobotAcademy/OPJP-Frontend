aef30772e602244353ce41db7134ea0c18d8f3c3
yooodleee <accia25@naver.com>
2024-12-24 14:50:16 +0900

    feat: [오민수] 로그인 화면 구현 [OPJP-2]

---
+++ src/account/pages/withdraw/AccountWithdrawPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-form class="reason-section">
+            <h3>서비스 품질 향상을 위해 탈퇴 사유를 선택해주세요.</h3>
+            <v-radio-group class="radio-group-section" v-model="selectedReason">
+                <v-radio
+                    v-for="(reason, index) in reasons"
+                    :key="index"
+                    :label="reason.label"
+                    :value="reason.value"
+                ></v-radio>
+            </v-radio-group>
+            <v-btn
+                
+        </v-form>
+    </v-container>
+</template>
